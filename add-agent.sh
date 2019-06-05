fff429a3ee41f569930e673b3cddbdca9326ccdc
Askotion <askotion@gmail.com>
2019-06-05 17:29:04 +0200

    Update add-agent.sh

--- add-agent.sh
+++ add-agent.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]] ; then
+    clear
+    echo 'You have to define a agent!'
+    echo 'Usage: ./add-agent.sh <agent> <server>'
+    exit 0
+fi
+
 agent=$1
+server=$2
 
-key=$(sed -n "/$agent/p" /var/ossec/etc/client.keys)
+key=$(sed -n "/$agent/p" agents.txt)
 
-echo $key
+ssh root@$server "source <(curl -s https://raw.githubusercontent.com/Askotion/snippets/master/install-ossec.sh) $key"
