24c0fc34fe6901e0ed9c55ab2462a260e9b223e3
Askotion <askotion@gmail.com>
2019-07-17 15:47:20 +0200

    use env variable for ip

--- floating-ip-hcloud.sh
+++ floating-ip-hcloud.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-IP="116.203.82.111"
-
-
-
 while true; do
-        if ping -c 1 $IP &> /dev/null
+        if fping -c1 -t2000 $IP &> /dev/null
         then
-          echo "Server online!"
+         sleep 1
         else
           echo "Server was down.. reassigning floating-ip!"
           hcloud floating-ip assign 84894 test2
-          sleep 10
           echo "done!"
         fi
-        sleep 1
 done
